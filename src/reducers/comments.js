c995aa5db1047865fe90728878dc0a304abb150e
hitesh <mukadder@bu.edu>
2017-10-15 18:27:23 -0400

    all sepcs done

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,4 +1,12 @@
+import {SAVE_COMMENT} from '../actions/types'
+
 export default function (state=[],action){
+    switch(action.type) {
+        case SAVE_COMMENT:
+//  state.concat([action.payload])
+        return [...state,action.payload]
+
+    }
 return state
 
 }
--- test/reducers/comments_test.js
+++ test/reducers/comments_test.js
@@ -5,11 +5,11 @@ import {SAVE_COMMENT} from '../../src/actions/types'
 
 describe('commments reducer ', () => {
     it('handles action with unknown type ',() =>{
-expect(commentReducer()).to.be.instanceOf(Array)
-expect(commentReducer()).to.be.eql([])
+//expect(commentReducer()).to.be.instanceOf(Array)
+expect(commentReducer(undefined,{})).to.be.eql([])
     })
     it('handles action with SAVE_COMMENT ',() =>{
         const action = {type:SAVE_COMMENT,payload:'new comment'}
-        expect(commentReducer([],action))
+        expect(commentReducer([],action)).to.eql(['new comment'])
     })
 })
