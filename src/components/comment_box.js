4b5e54db989f1b98f0ea0045fb67b8bad7141253
hitesh <mukadder@bu.edu>
2017-10-14 21:00:49 -0400

    component nadded

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,12 +1,17 @@
 import React , {Component} from 'react'
 
 export default class CommentBox extends Component  {
-
+    constructor(props){
+    super(props)
+    this.state = { comment:''}
+}
+    onHandleChange(event){
+this.setState({comment:event.target.value })
+    }
     render () {
-        return ( <div
-
-className ="comment-box">
-            <textarea />
+        return ( 
+        <div className ="comment-box">
+            <textarea  value ={this.state.comment}onChange = {this.onHandleChange.bind(this)}/>
             <button>Submit comment </button>
             </div>)
     }
--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -19,4 +19,18 @@ it('has a text area' ,() => {
     //const component = renderComponent(CommentBox);
     expect(component.find('button')).to.exist;
     });
+describe ('entering some text ',() => {
+    beforeEach(() => {
+        // change event is triggered the event called change event  a fake event new comment is new text entered 
+        component.find('textarea').simulate('change','new comment')
+    })
+    it('show text that is text area  ',() =>{
+
+        expect(component.find('textarea')).to.have.value('new comment')
+ })
+ it('when submitted clears the input ' ,() => {
+        
+     })
+})
+    
 })
